78106b15a9f48d3c3a2693121c70e2534b6b7c38
Zed Tan <zed@shootbird.work>
2020-12-23 22:21:11 +0100

    modified main.rs, moving day1 solution to its own function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use ac::*;
 
-fn main() {
+fn day1() -> u64 {
     //ac::ask_2020();
     let filename = "src/data/01.csv";
     //std::io::stdin().read_line(&mut filename).unwrap();
     let list_of_strings = read_numlist_from_file(get_filepath(filename));
     
+    let mut result: u64 = 0;
+
     for idx in 1..list_of_strings.len() {
         if idx == list_of_strings.len() {
             break;
@@ -20,11 +22,16 @@ fn main() {
                 let num3 = list_of_strings[i];
 
                 if is_sum_2020(vec![num1, num2, num3]) {
-                    println!("Is sum 2020?: {}", is_sum_2020(vec![num1, num2, num3]));
-                    println!("{}", get_product(vec![num1, num2, num3]))            
+                    result = get_product(vec![num1, num2, num3])
                 }
             }
         }
     }
+    
+    return result
+}
+
+fn main() {
+    println!("{}", day1());
 }
 
