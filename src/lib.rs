a981cdd56fccad6a2955d256ff98f65a925d0f01
Zed Tan <zed@shootbird.work>
2020-12-25 05:46:01 +0100

    reorganized code

--- src/lib.rs
+++ src/lib.rs
@@ -6,85 +6,111 @@ mod tests {
 
     #[test]
     fn sum_2020() {
-        assert_eq!(is_sum_2020(vec![1010, 1010]), true)
+        assert_eq!(Utils::is_sum_2020(vec![1010, 1010]), true)
     }
 
     #[test]
     fn get_2020_product() {
-        assert_eq!(get_product(vec![5435345, 345345]), 1877069219025)
+        assert_eq!(Utils::get_product(vec![5435345, 345345]), 1877069219025)
     }
 }
 
-pub fn is_sum_2020(numlist: Vec<u64>) -> bool {
-    let mut total: u64 = numlist[0];
-    for i in 1..numlist.len() {
-        total = total + numlist[i];
-    }
-    total == 2020
+
+#[derive(Debug)]
+pub struct PuzzleInput {
+    content: Vec<String>
 }
 
-pub fn get_product(numlist: Vec<u64>) -> u64 {
-    let mut product: u64 = numlist[0];
-    for i in 1..numlist.len() {
-        if i <= numlist.len() {
-            product = product * numlist[i]; 
-        }
-    }
-    product
-}
-
-fn parse_number(line: String) -> u64 {
-    line.trim().parse::<u64>().unwrap()
-}
-
-pub fn get_filepath(filename: &str) -> std::path::PathBuf {
-    let filepath = std::env::current_dir()
-        .expect("Invalid path to file")
-        .join(std::path::Path::new(filename));
-
-    if !filepath.is_file() {
-        panic!("Cannot read file")
-    }
-
-    filepath
-}
-
-pub fn read_numlist_from_file(filepath: std::path::PathBuf) -> Vec<u64> {
-    let file = std::fs::File::open(filepath)
+impl PuzzleInput {
+    pub fn from_file(filepath: std::path::PathBuf) -> Self {
+        let file = std::fs::File::open(filepath)
         .unwrap();
-
-    let lines = BufReader::new(file).lines();
-
-    let mut list: Vec<u64> = Vec::new();
-
+        
+        let lines = BufReader::new(file).lines();
+        
+        let mut list: Vec<String> = Vec::new();
+        
+        
+        for (_, line) in lines.enumerate() {        
+            list.push(line.unwrap());
+        }
+        
+        return Self {content: list}
+    }
     
-    for (_, line) in lines.enumerate() {
-        let thisline = line.unwrap();
+    pub fn parse_content_to_numbers(filepath: std::path::PathBuf) -> Vec<u64> {
+        let content = &Self::from_file(filepath);
+        let mut list: Vec<u64> = Vec::new();
+        
+        for line in 1..content.content.len() {
+            let thisline = &content.content[line-1];
+            
+            for c in thisline.chars() {
+                if !c.is_ascii_digit() {
+                    eprintln!("Invalid number: {}", &thisline)
+                }
+            }
+            
+            let resulting_line: u64 = thisline.parse().unwrap();
+            
+            list.push(resulting_line);
+        }
+        
+        list
+    }
+    
+}
 
-        for c in thisline.chars() {
-            if !c.is_ascii_digit() {
-                eprintln!("Invalid number: {}", &thisline)
+pub struct Utils;
+
+impl Utils {
+
+    pub fn get_filepath(filename: &str) -> std::path::PathBuf {
+        let filepath = std::env::current_dir()
+            .expect("Invalid path to file")
+            .join(std::path::Path::new(filename));
+
+        if !filepath.is_file() {
+            panic!("Cannot read file")
+        }
+
+        filepath
+    }
+
+    pub fn is_sum_2020(numlist: Vec<u64>) -> bool {
+        let mut total: u64 = numlist[0];
+        for i in 1..numlist.len() {
+            total = total + numlist[i];
+        }
+        total == 2020
+    }
+
+    pub fn get_product(numlist: Vec<u64>) -> u64 {
+        let mut product: u64 = numlist[0];
+        for i in 1..numlist.len() {
+            if i <= numlist.len() {
+                product = product * numlist[i]; 
             }
         }
-
-        let resulting_line: u64 = thisline.parse().unwrap();
-        
-        list.push(resulting_line);
+        product
     }
 
-    list
+    fn parse_number(line: String) -> u64 {
+        line.trim().parse::<u64>().unwrap()
+    }
+
+    pub fn ask_2020() {
+        let mut line1 = String::new();
+        let mut line2 = String::new();
+        
+        println!("Enter your first number:");
+        std::io::stdin().read_line(&mut line1).unwrap();
+
+        println!("Enter your second number:");
+        std::io::stdin().read_line(&mut line2).unwrap();
+
+        println!("{}", Utils::is_sum_2020(vec![Utils::parse_number(line1), Utils::parse_number(line2)]));
+    }
 }
 
-pub fn ask_2020() {
-    let mut line1 = String::new();
-    let mut line2 = String::new();
-    
-    println!("Enter your first number:");
-    std::io::stdin().read_line(&mut line1).unwrap();
-
-    println!("Enter your second number:");
-    std::io::stdin().read_line(&mut line2).unwrap();
-
-    println!("{}", is_sum_2020(vec![parse_number(line1), parse_number(line2)]));
-}
 
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use ac::*;
 
 fn day1() -> u64 {
-    //ac::ask_2020();
     let filename = "src/data/01.csv";
-    //std::io::stdin().read_line(&mut filename).unwrap();
-    let list_of_strings = read_numlist_from_file(get_filepath(filename));
+    let numlist = PuzzleInput::parse_content_to_numbers(Utils::get_filepath(filename));
     
     let mut result: u64 = 0;
 
-    for idx in 1..list_of_strings.len() {
-        if idx == list_of_strings.len() {
+    for idx in 1..numlist.len() {
+        if idx == numlist.len() {
             break;
         }
 
-        let num1 = list_of_strings[idx-1];
+        let num1 = numlist[idx-1];
 
-        for i in 1..list_of_strings.len() {
-            let num2 = list_of_strings[i];
+        for i in 1..numlist.len() {
+            let num2 = numlist[i];
 
-            for i in 1..list_of_strings.len() {
-                let num3 = list_of_strings[i];
+            for i in 1..numlist.len() {
+                let num3 = numlist[i];
 
-                if is_sum_2020(vec![num1, num2, num3]) {
-                    result = get_product(vec![num1, num2, num3])
+                if Utils::is_sum_2020(vec![num1, num2, num3]) {
+                    result = Utils::get_product(vec![num1, num2, num3])
                 }
             }
         }
