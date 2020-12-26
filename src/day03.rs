edfe73aabf4e8f8008413ae99c5a1d0c9e707dbd
Zed Tan <zed@shootbird.work>
2020-12-26 06:40:42 +0100

    implemented extending the map horizontally, with tests

--- src/day03.rs
+++ src/day03.rs
@@ -4,9 +4,8 @@ use std::io::{prelude::* , BufReader};
 mod tests {
     use super::*;
 
-    #[test]
-    fn ingest_maptile() {
-        let expected = MapTile {
+    fn expected_maptile() -> MapTile {
+        MapTile {
             content:  vec![
                 MapRow {
                     content: vec!['.', '#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.'] },
@@ -17,25 +16,67 @@ mod tests {
                 MapRow {
                     content: vec!['#', '.', '#', '.', '#', '.', '.', '.', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#'] }
             ],
-        };
-
-        assert_eq!(expected, MapTile::new(crate::utils::get_filepath("src/tests/test_map_data.txt")))
+        }
     }
 
+    #[test]
+    fn ingest_maptile() {
+        assert_eq!(expected_maptile(), MapTile::new(crate::utils::get_filepath("src/tests/test_map_data.txt")))
+    }
+
+    #[test]
+    fn map_new() {
+        let expected = Map {
+            content: vec![expected_maptile()],
+        };
+
+        assert_eq!(expected, Map::new(crate::utils::get_filepath("src/tests/test_map_data.txt")));
+    }
+
+    #[test]
+    fn map_extend() {
+        let expected = Map {
+            content: vec![expected_maptile(), expected_maptile()]
+        };
+
+        let mut initial_map = Map {
+            content: vec![expected_maptile()]
+        };
+
+        assert_eq!(expected, initial_map.extend());
+    }
 }
 
+#[derive(Debug, PartialEq, Clone)]
 struct Map  {
     content: Vec<MapTile>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct MapTile{
     content: Vec<MapRow>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct MapRow {
     content: Vec<char>,
+}
+
+impl Map {
+    fn new(filepath: std::path::PathBuf) -> Self {
+        Self {
+            content: vec![MapTile::new(filepath)],
+        }
+    }
+
+    fn extend(&self) -> Self{
+        let mut newmap: Map = self.clone();
+        let mut init_maptile: MapTile = self.content[0].clone();
+
+        newmap.content.push(init_maptile);
+
+        newmap
+    }
 }
 
 impl MapTile {
